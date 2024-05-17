5dac8c9ecffab127d88f9217b5c84a5873d658f2
DucKien <91297650+DuckienDev@users.noreply.github.com>
2024-05-17 11:16:18 +0700

    gg map

--- travelapp/ios/Runner/AppDelegate.swift
+++ travelapp/ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import GoogleMaps
+
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +9,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDZUB2_M3J_jUi4H2hll8a6shR4R2noYIU")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- travelapp/lib/screen/map_screen.dart
+++ travelapp/lib/screen/map_screen.dart
@@ -1,10 +1,34 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 import 'package:travelapp/home_main.dart';
 import 'package:travelapp/widget/HomeWidget/home_appbar_seach.dart';
 
-class MapScreenn extends StatelessWidget {
+class MapScreenn extends StatefulWidget {
   const MapScreenn({super.key});
+
+  @override
+  State<MapScreenn> createState() => _MapScreennState();
+}
+
+class _MapScreennState extends State<MapScreenn> {
+  final Location _locationControler = new Location();
+
+  final Completer<GoogleMapController> _mapControler =
+      Completer<GoogleMapController>();
+
+  static LatLng _pHaNoi = LatLng(21.02873, 105.85074);
+  static LatLng _pTPHue = LatLng(16.46371, 107.59084);
+
+  LatLng? _currentP = null;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationUpdates();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,14 +54,75 @@ class MapScreenn extends StatelessWidget {
           ),
         ),
       ),
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Image.asset(
-          'images/Logo/ggmapImg.png',
-          fit: BoxFit.cover,
-        ),
-      ),
+      body: _currentP == null
+          ? Center(child: Text('Loading...'))
+          : GoogleMap(
+              onMapCreated: ((GoogleMapController controller) =>
+                  _mapControler.complete(controller)),
+              initialCameraPosition: CameraPosition(
+                target: _currentP!,
+                zoom: 12,
+              ),
+              markers: {
+                Marker(
+                  markerId: MarkerId('_currentLocation'),
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: _currentP!,
+                ),
+                Marker(
+                    markerId: MarkerId('_sourcleLocation'),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: _pHaNoi),
+                Marker(
+                  markerId: MarkerId('_destionationLocation'),
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: _pTPHue,
+                )
+              },
+            ),
+    );
+  }
+
+  Future<void> _cameraToPosition(LatLng pos) async {
+    final GoogleMapController controller = await _mapControler.future;
+    CameraPosition _newCameraPosition = CameraPosition(target: pos, zoom: 12);
+
+    await controller.animateCamera(
+      CameraUpdate.newCameraPosition(_newCameraPosition),
+    );
+  }
+
+  Future<void> getLocationUpdates() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await _locationControler.serviceEnabled();
+    if (_serviceEnabled) {
+      _serviceEnabled = await _locationControler.requestService();
+    } else {
+      return;
+    }
+
+    _permissionGranted = await _locationControler.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _locationControler.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+    _locationControler.onLocationChanged.listen(
+      (LocationData currentLocation) {
+        if (currentLocation.latitude != null &&
+            currentLocation.longitude != null) {
+          setState(
+            () {
+              _currentP =
+                  LatLng(currentLocation.latitude!, currentLocation.longitude!);
+              _cameraToPosition(_currentP!);
+            },
+          );
+        }
+      },
     );
   }
 }
