969b8a3362951849582b269f981616c103ae7e55
DucKien <91297650+DuckienDev@users.noreply.github.com>
2024-06-14 20:33:38 +0700

    .

--- travelapp/lib/screen/notification_screen.dart
+++ travelapp/lib/screen/notification_screen.dart
@@ -9,87 +9,93 @@ class NotificationScreenn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final message = ModalRoute.of(context)!.settings.arguments as RemoteMessage;
+    final message =
+        ModalRoute.of(context)?.settings.arguments as RemoteMessage?;
     return Scaffold(
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(90),
-          child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 15, right: 15),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          Navigator.push(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(90),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15, right: 15),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HomePage(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        width: 45,
+                        height: 45,
+                        decoration: BoxDecoration(
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.black,
+                                blurRadius: 2,
+                              )
+                            ],
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white),
+                        child: const Icon(Icons.arrow_back_ios_new, size: 20),
+                      ),
+                    ),
+                    const Text(
+                      'Notification',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) => HomePage(),
-                            ),
-                          );
-                        },
-                        child: Container(
-                          width: 45,
-                          height: 45,
-                          decoration: BoxDecoration(
-                              boxShadow: const [
-                                BoxShadow(
-                                  color: Colors.black,
-                                  blurRadius: 2,
-                                )
-                              ],
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white),
-                          child: const Icon(Icons.arrow_back_ios_new, size: 20),
+                                builder: (context) => const SettingScreen()));
+                      },
+                      child: Container(
+                        width: 45,
+                        height: 45,
+                        decoration: BoxDecoration(
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.black,
+                                blurRadius: 3,
+                              )
+                            ],
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white),
+                        child: const Icon(
+                          Icons.settings,
+                          size: 20,
+                          color: Colors.black,
                         ),
                       ),
-                      const Text(
-                        'Notification',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.w600),
-                      ),
-                      InkWell(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const SettingScreen()));
-                        },
-                        child: Container(
-                          width: 45,
-                          height: 45,
-                          decoration: BoxDecoration(
-                              boxShadow: const [
-                                BoxShadow(
-                                  color: Colors.black,
-                                  blurRadius: 3,
-                                )
-                              ],
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white),
-                          child: const Icon(
-                            Icons.settings,
-                            size: 20,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 10)
-                ],
-              ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10)
+              ],
             ),
           ),
         ),
-        body: Column(
-          children: [
-            Text(message.notification!.title.toString()),
-            Text(message.notification!.body.toString()),
-            Text(message.data.toString()),
-          ],
-        ));
+      ),
+      body: (message != null)
+          ? Column(
+              children: [
+                Text(message.notification!.title.toString()),
+                Text(message.notification!.body.toString()),
+                Text(message.data.toString()),
+              ],
+            )
+          : Center(
+              child: Text("Don't have notification."),
+            ),
+    );
   }
 }
